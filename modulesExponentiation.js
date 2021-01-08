64173ba9cc2922195b039d9c900aedff6e68bd63
Eugene Matskin <eugene@matskin.net>
2021-01-08 09:26:26 +0200

    Modification task 1 and added task 2

---
+++ modulesExponentiation.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const exp = a ** b;
+const mod = a % b;
+
+document.write('<br><h3>Task 2 | Modules and exponentiation</h3>');
+document.write('Exponentiation = ' + exp + '<br>');
+document.write('Modules = ' + mod + '<br>');
--- numbersOperators.js
+++ numbersOperators.js
@@ -9,11 +9,12 @@ const division = a / b;
 const multiplication = a * b;
 
 // use variables 'a' & 'b'
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 
+document.write('<h3>Task 1 | Numbers operators</h3>');
 document.write('a = ' + a + '<br>');
-document.write('b = ' + b + '<br>');
+document.write('b = ' + b + '<br><br>');
 document.write('addition = ' + addition + '<br>');
 document.write('subtraction = ' + subtraction + '<br>');
 document.write('division = ' + division + '<br>');
