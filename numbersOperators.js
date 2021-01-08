6eb0c818d428487a56029187aea82b067e19ce39
Eugene Matskin <eugene@matskin.net>
2021-01-08 09:02:47 +0200

    Added task - numbersOpertors

---
+++ numbersOperators.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const a = 6;
+const b = 5;
+
+const addition = a + b;
+const subtraction = a - b;
+const division = a / b;
+const multiplication = a * b;
+
+// use variables 'a' & 'b'
+console.log(a);
+console.log(b);
+
+document.write('a = ' + a + '<br>');
+document.write('b = ' + b + '<br>');
+document.write('addition = ' + addition + '<br>');
+document.write('subtraction = ' + subtraction + '<br>');
+document.write('division = ' + division + '<br>');
+document.write('multiplication = ' + multiplication + '<br>');
