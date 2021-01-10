e89a6abfd596fe80e36db8c77bfd6b5e6b43251e
Eugene Matskin <eugene@matskin.net>
2021-01-10 18:57:50 +0200

    Ways to create functions

---
+++ arrowFunction.js
@@ -0,0 +1,17 @@
+function getCircleSquare1(r) {
+    return Math.PI * r * r;
+};
+
+const getCircleSquare2 = function(r) {
+    return Math.PI * r * r;
+};
+
+const getCircleSquare3 = (r) => {
+    return Math.PI * r * r;
+};
+
+const getCircleSquare4 = (r) => Math.PI * r * r;
+const getCircleSquare5 = r => Math.PI * r * r;
+
+const square = getCircleSquare5(1);
+console.log(square);
