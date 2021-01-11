8ebcbe76ccce0045bada1ab053b17c00d743649d
Eugene Matskin <eugene@matskin.net>
2021-01-11 18:39:33 +0200

    Added function canBuyBeer

---
+++ canBuyBeer.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @param {number} age
+ *
+ * @returns string
+ */
+
+// declare your function below
+
+function canBuyBeer(age) {
+  if (age >= 18) {
+    return 'You can buy beer';
+  } else {
+    return 'You can not buy beer';
+  }
+}
