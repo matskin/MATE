25c74f1fd5f113a1c7fb3c48e68375b99f1a8007
Eugene Matskin <eugene@matskin.net>
2021-01-12 13:03:32 +0200

    Modify function getTipsRating

--- getTipsRating.js
+++ getTipsRating.js
@@ -9,23 +9,21 @@
 // write code below this line
 
 function getTipsRating(amount) {
-  if (amount === 0) {
-    return 'terrible';
-  }
-
-  if (amount > 0 && amount <= 10) {
-    return 'poor';
-  }
-
-  if (amount > 10 && amount <= 20) {
-    return 'good';
-  }
-
-  if (amount > 20 && amount <= 50) {
-    return 'great';
-  }
-
   if (amount > 50) {
     return 'excellent';
   }
+
+  if (amount > 20) {
+    return 'great';
+  }
+
+  if (amount > 10) {
+    return 'good';
+  }
+
+  if (amount > 0) {
+    return 'poor';
+  }
+
+    return 'terrible';
 }
