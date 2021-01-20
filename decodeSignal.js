d0c61555fde0c9977f663e0fa069c4d55a817d29
Eugene Matskin <eugene@matskin.net>
2021-01-20 21:53:04 +0200

    Added function decodeSignal

---
+++ decodeSignal.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+ * @param input
+ *
+ * @returns number
+ */
+
+function decodeSignal(input) {
+  // write code here
+  if (!!input === false) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
