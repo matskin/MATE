8cdcb1b5b9d4a9daa3a2188ff8fedb752cba6183
Eugene Matskin <eugene@matskin.net>
2021-01-22 20:31:10 +0200

    Added function isEven

---
+++ isEven.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+ * @param {number} number
+ *
+ * @returns boolean
+ */
+
+function isEven(number) {
+  // write code here
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
