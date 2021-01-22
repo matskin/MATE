c5ea180ea60159ad4b46776c14bac7e0bc3960a3
Eugene Matskin <eugene@matskin.net>
2021-01-22 23:03:05 +0200

    Added function countNetworking

---
+++ countNetworking.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/*
+ * @param {number} K
+ * @param {number} N
+ *
+ * @returns number
+ */
+
+function countNetworking(K, N) {
+  // write code here
+  return Math.floor((12 - K) / N);
+}
