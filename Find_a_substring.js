bf59d4619999f4b077b5f0905ae34269a6a7122e
Eugene Matskin <eugene@matskin.net>
2021-01-23 22:23:48 +0200

    Find_a_substring

---
+++ Find_a_substring.js
@@ -0,0 +1,10 @@
+const word = 'absdebc';
+const part = 'bc';
+
+console.log(
+  word.includes(part),
+  word.indexOf(part),
+  word.lastIndexOf(part),
+  word.startsWith(part),
+  word.endsWith(part),
+);
